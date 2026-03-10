6c99e8cc15c2207e0c14f73517a79319b9b36def
minaC128 <mina970128@gmail.com>
2026-03-10 11:45:48 +0800

    Delete damalive-app-responsive directory

--- damalive-app-responsive/find-models.ts
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-const data = JSON.parse(fs.readFileSync('models_out.txt', 'utf8'));
-data.models.forEach((m: any) => {
-    if (m.supportedGenerationMethods.includes('generateContent')) {
-        console.log(m.name);
-    }
-});
